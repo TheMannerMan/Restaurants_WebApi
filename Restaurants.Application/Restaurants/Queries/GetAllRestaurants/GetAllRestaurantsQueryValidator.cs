b618b3a60daa1d64d5e9e94163fb7342aebb0ef6
Henri Lehtonen <henri.lehtonen.87@gmail.com>
2026-01-06 15:06:20 +0100

    76: Add query validator for GetAllRestaurants endpoint
    Introduced GetAllRestaurantsQueryValidator using FluentValidation to enforce allowed page sizes and minimum page number.

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private int[] allowedPageSize = [5, 10, 15, 30];
+    
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(r => r.PageSize)
+            .Must(value => allowedPageSize.Contains(value))
+            .WithMessage($"Page size must be in [{string.Join(",", allowedPageSize)}]");
+    }
+}
