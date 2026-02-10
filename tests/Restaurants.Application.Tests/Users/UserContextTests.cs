7bdea668e710ac5804514dda5539789338c3c673
Henri Lehtonen <146133540+TheMannerMan@users.noreply.github.com>
2026-02-10 11:04:53 +0100

    Update expected user ID in UserContextTests

--- tests/Restaurants.Application.Tests/Users/UserContextTests.cs
+++ tests/Restaurants.Application.Tests/Users/UserContextTests.cs
@@ -44,7 +44,7 @@ public class UserContextTests
         var currentUser = userContext.GetCurrentUser();
         // Assert
         currentUser.Should().NotBeNull();
-        currentUser.Id.Should().Be("1");
+        currentUser.Id.Should().Be("2");
         currentUser.Email.Should().Be("test@test.com");
         currentUser.Roles.Should().ContainInOrder(UserRoles.Admin, UserRoles.User);
         currentUser.Nationality.Should().Be("German");
